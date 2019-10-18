750c120023909649ae2aa3eb34205184f2f603bc
Zachariah Ahrendsen <50559548+zahrendsen@users.noreply.github.com>
2019-10-17 18:38:32 -0600

    Add files via upload
    added joins and views to display data in requested forms

--- disk_inventoryZA_dbscript.sql
+++ disk_inventoryZA_dbscript.sql
@@ -3,6 +3,7 @@
 /*                                                                        */ 
 /*  10/3/19		Zach Ahrendsen		created database, readonly user,      */ 
 /*  10/11/19	Zach Ahrendsen		inserted and modfied table data       */ 
+/*  10/17/19	Zach Ahrendsen		added joins and views for data		  */ 
 /*                                                                        */ 
 /**************************************************************************/
 
@@ -105,26 +106,25 @@ values ('Electronic'), ('Rock'), ('Jazz'), ('Blues'), ('Country'), ('Indie'), ('
 
 --Inserting unique data into table
 insert into disk
-values('Sgt. Pepper’s Lonely Hearts Club Band', '1967', 2, 1, 2),
-	('The Wall', '1979', 2, 1, 2),
-	('Bad', '1987', 2, 1, 7),
-	('Let’s Talk About Love', '1997', 2, 1, 7),
-	('Born in the U.S.A.', '1984', 2, 1, 2),
-	('Supernatural', '1999', 2, 1, 16),
-	('Metallica', '1991', 2, 1, 14),
-	('Supernatural', '1999', 2, 1, 16),
-	('Appetite for Destruction', '1987', 2, 1, 2),
-	('Abbey Road', '1969', 2, 1, 2),
-	('Help', '1965', 2, 1, 2),
-	('The Godfather', '1972', 2, 2, 21),
-	('The Dark Knight', '2008', 2, 2, 21),
-	('Citizen Kane', '1941', 2, 2, 21),
-	('Titanic', '1997', 2, 2, 21),
-	('At Folsom Prison', '1968', 2, 1, 5),
-	('Ropin the Wind', '1991', 2, 1, 5),
-	('All Eyez on Me', '1996', 2, 1, 12),
-	('Ready to Die', '1994', 2, 1, 12),
-	('Oracular Spectacular', '2007', 2, 1, 6);
+values('Sgt. Pepper’s Lonely Hearts Club Band', '5/26/1967', 2, 1, 2),
+	('The Wall', '11/30/1979', 2, 1, 2),
+	('Bad', '8/31/1987', 2, 1, 7),
+	('Let’s Talk About Love', '11/14/1997', 2, 1, 7),
+	('Born in the U.S.A.', '6/4/1984', 2, 1, 2),
+	('Supernatural', '6/15/1999', 2, 1, 16),
+	('Metallica', '8/12/1991', 2, 1, 14),
+	('Appetite for Destruction', '7/21/1987', 2, 1, 2),
+	('Abbey Road', '9/26/1969', 2, 1, 2),
+	('Help', '8/6/1965', 2, 1, 2),
+	('The Godfather', '3/15/1972', 2, 2, 21),
+	('The Dark Knight', '7/18/2008', 2, 2, 21),
+	('Citizen Kane', '9/5/1941', 2, 2, 21),
+	('Titanic', '12/19/1997', 2, 2, 21),
+	('At Folsom Prison', '5/1/1968', 2, 1, 5),
+	('Ropin the Wind', '9/2/1991', 2, 1, 5),
+	('All Eyez on Me', '2/13/1996', 2, 1, 12),
+	('Ready to Die', '9/13/1994', 2, 1, 12),
+	('Oracular Spectacular', '10/2/2007', 2, 1, 6);
 
 -- Updated genre_id for specific disk
 update disk
@@ -195,12 +195,12 @@ values(5, 10, '12/8/2017', '12/10/2017'),
 	(10, 10, '10/8/2019', '10/9/2019'),
 	(19, 10, '10/9/2019', NULL),
 	(17, 10, '07/8/2017', '07/21/2017'),
-	(20, 10, '05/09/2016', '07/8/2016'),
-	(20, 2, '12/8/2016', '12/12/2016'),
+	(2, 10, '05/09/2016', '07/8/2016'),
+	(1, 2, '12/8/2016', '12/12/2016'),
 	(8, 10, '09/8/2019', NULL),
 	(16, 1, '10/8/2017', '01/01/2018'),
 	(16, 4, '01/20/2018', NULL),
-	(5, 10, '08/8/2019', NULL),
+	(5, 10, '08/8/2019', '08/9/2019'),
 	(3, 12, '02/01/2019', '02/22/2019'),
 	(5, 8, '05/22/2017', '09/22/2017'),
 	(5, 10, '01/22/2018', '06/05/2018'),
@@ -210,15 +210,72 @@ values(5, 10, '12/8/2017', '12/10/2017'),
 	(9, 3, '06/16/2019', '08/23/2019'),
 	(4, 4, '07/13/2019', '09/17/2019'),
 	(5, 2, '10/13/2019', NULL);
+	 
 
 --Inserting unique data into table
 
 insert into artist_disk
-values(1,1), (2,2), (3,3), (4,4), (5,5), (6,6), (7,7), (8,6), (9,8), (10,1), (11,1),
-	(16,9), (17,10), (18,11), (19,12), (20,13), (1,18), (1,19), (1,20),  (10,18), 
-	(10,19), (10,20), (11,18), (11,19), (11,20);
+values(1,1), (2,2), (3,3), (4,4), (5,5), (6,6), (7,7), (8,8), (9,1), (10,1),
+	(16,10), (17,11), (18,12), (19,13), (1,18), (1,19), (1,20),  (10,18), 
+	(10,19), (10,20), (9,18), (9,19), (9,20);
 
 -- Query to list the disks that are on loan and have not been returned
 select *
 from borrower_trans
 where returned_date is null;
+
+--Shows the disks in the database associated with Individual artists only.
+select  disk_name as 'Disk Name', convert(varchar(10), release_date, 101) as 'Release Date', fname as 'First Name', lname as 'Last Names'
+from disk
+join artist_disk on disk.disk_id = artist_disk.disk_id
+join artist on artist.artist_id = artist_disk.artist_id
+where artist_type_id = 1
+order by lname, fname, disk_name;
+go
+
+--View that shows individual artists names
+drop view if exists View_Individual_Artist;
+go
+create view View_Individual_Artist as 
+	select artist_id, fname, lname
+	from artist
+	where artist_type_id = 1 
+go
+select fname as 'First Name' , lname as 'Last Name'
+from View_Individual_Artist
+
+--Shows the disks in the database associated with group artists only.
+select disk_name as 'Disk Name', convert(varchar(10), release_date, 101) as 'Release Date', fname as 'Group Name'
+from disk
+join artist_disk on disk.disk_id = artist_disk.disk_id
+join artist on artist.artist_id = artist_disk.artist_id
+where artist.artist_id not in 
+ (select artist_id from View_Individual_Artist)
+ order by fname, disk_name;
+
+ --shows which disks have been borrowed and who has borrowed them.
+ select fname as 'First Name', lname as 'Last Name', disk_name as 'Disk Name', 
+	convert(varchar(10), borrowed_date, 120) as 'Borrowed Date',
+	convert(varchar(10), returned_date, 120) as 'Return Date'
+ from borrower
+ join borrower_trans on borrower.borrower_id = borrower_trans.borrower_id
+ join disk on disk.disk_id = borrower_trans.disk_id
+ order by lname, fname, disk_name, borrowed_date, returned_date;
+
+ --shows the number of times a specific disk has been borrowed
+ select disk.disk_id as 'Disk ID', disk_name as 'Disk Name', count(*) as 'Times Borrowed'
+ from disk
+ join borrower_trans on disk.disk_id = borrower_trans.disk_id
+ group by disk.disk_id, disk_name
+ order by disk.disk_id;
+
+ --shows disks currently being borrowed/on loan and who has them.
+select disk_name as 'Disk Name', 
+	convert(varchar(10), borrowed_date, 120) as 'Borrowed',
+	convert(varchar(10), returned_date, 120) as 'Return',
+	lname as 'Last Name' 
+from disk
+join borrower_trans on disk.disk_id = borrower_trans.disk_id
+join borrower on borrower.borrower_id = borrower_trans.borrower_id
+where returned_date is null
+order by disk_name;
